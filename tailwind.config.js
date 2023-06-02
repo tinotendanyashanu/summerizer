9706abf025f98fafb8c70da5d3e96ae2e50c6660
tinotendnyashanu <tinotendnyash@gmail.com>
2023-06-02 17:46:06 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "Sans-serif"],
+        inter: ["Inter", "Sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
